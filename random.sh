4e46485ea2158d86fb97226ebaee62e4e62f9fdc
Alex Xia <61260409+alexalex1228@users.noreply.github.com>
2020-03-12 19:48:03 +0800

    Add files via upload

---
+++ random.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+n=$(( RANDOM % 100 ))
+if [[ $n -eq 42 ]];then
+	echo "Something went wrong"
+	echo "The error was using magic numbers"
+	exit 1
+fi
+
+echo "Everything went according to plan"
